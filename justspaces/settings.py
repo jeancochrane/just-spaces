443b77ac8e88cb1d3b788c4038e7d88bd790e076
Jean Cochrane <jean@jeancochrane.com>
2019-05-10 13:23:14 -0400

    Containerize app

--- justspaces/settings.py
+++ justspaces/settings.py
@@ -11,15 +11,25 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-from .local_settings import SECRET_KEY, DEBUG, ALLOWED_HOSTS, DATABASES
 import gettext
+
+SECRET_KEY = os.getenv('SECRET_KEY', 'foobarbaz')
+DEBUG = os.getenv('DEBUG', True)
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', [])
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': os.getenv('DBNAME', 'just-spaces'),
+        'USER': os.getenv('DBUSER', 'postgres'),
+        'PASSWORD': os.getenv('DBPASSWORD', ''),
+        'HOST': os.getenv('DBHOST', 'localhost'),
+        'PORT': os.getenv('DBPORT', '5432'),
+    }
+}
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Fix Heroku GDAL path problems
-GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
-GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
--- scripts/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cp justspaces/local_settings.example.py justspaces/local_settings.py
-pytest
