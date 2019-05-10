d913a014c738fe458e719ac5fcc7a98fda9af969
Jean Cochrane <jean@jeancochrane.com>
2019-05-10 17:43:56 -0400

    Remove Whitenoise compression for DataTables support

--- justspaces/settings.py
+++ justspaces/settings.py
@@ -180,7 +180,6 @@ AUTH_USER_MODEL = "users.JustSpacesUser"
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = 'surveys-list-run'
 
